15a005ab29bccd52aa1ed6320cf0999460c11f19
Räisänen Matias <a1700451@myy.haaga-helia.fi>
2018-01-17 11:11:29 +0200

    Exercise 1 done

--- demo/src/main/java/com/example/demo/controller/HelloController.java
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -7,8 +7,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @ResponseBody
 public class HelloController {
-	@RequestMapping("*")
-	public String hello() {
-		return "Hello world!";
+	@RequestMapping("/index")
+	public String helloMain() {
+		return "This is the main page";
+	}
+	
+	@RequestMapping("/contact")
+	public String helloContact() {
+		return "This is the contact page";
 	}
 }
