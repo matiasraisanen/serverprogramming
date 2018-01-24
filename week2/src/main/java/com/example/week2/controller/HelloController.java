6e76a6a687c77fdfc2bc7972f848ed8a6e2e849e
matiasraisanen <33057754+matiasraisanen@users.noreply.github.com>
2018-01-24 11:00:47 +0200

    harjoitus 2

--- week2/src/main/java/com/example/week2/controller/HelloController.java
+++ week2/src/main/java/com/example/week2/controller/HelloController.java
@@ -1,16 +1,29 @@
 package com.example.week2.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import com.example.week2.domain.Student;
 
 @Controller
 public class HelloController {
 	@RequestMapping("/hello")
-	public String home(@RequestParam(value="name") String name, @RequestParam(value="age") String age, Model model) {
-		model.addAttribute("name", name);
-		model.addAttribute("age", age);
+	public String students(Model model) {
+		Student student1 = new Student("Kate", "Cole", "kate@cole.com");
+		Student student2 = new Student("Dan", "Brown", "dan@brown.com");
+		Student student3 = new Student("Mike", "Mars", "mike@mars.com");
+
+		List<Student> list = new ArrayList<>();
+		list.add(student1);
+		list.add(student2);
+		list.add(student3);
+
+		model.addAttribute("students", list);
+
 		return "hello";
 	}
+
 }
